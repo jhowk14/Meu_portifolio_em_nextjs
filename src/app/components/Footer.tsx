f508fb275baf380aaefd0f4b986d70f9fdd59534
unknown <jonathanperozi16@gmail.com>
2023-07-14 17:26:04 -0300

    enviando pagina base fase de produção dos projetos em telas

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="w-full text-white p-4 text-center">
+        <p>Programado por Jonathan Henrique Perozi Lourenço &copy; 2023</p>
+      </footer>
+    </>
+  );
+}
