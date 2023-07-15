15979ef2d61448342beeee8423690d42c132850e
jhowk14 <jonathanperozi16@gmail.com>
2023-07-14 22:42:34 -0300

    update header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return <>
     <header className=" text-white p-4">
-    <ul className="space-x-10 flex justify-center text-3xl font-bold">
+    <ul className="space-x-10 flex justify-center text-xl font-bold">
       <li  className="hover:text-gray-400"><Link href="/">Inicio</Link></li>
       <li className="hover:text-gray-400 cursor-pointer"><Link href="/about">Sobre min</Link></li>
       <li><a href="/projetos" className="hover:text-gray-400">Projetos</a></li>
