c8e4da442f939f25b670066c9041760e76144d4a
jhowk14 <jonathanperozi16@gmail.com>
2023-07-14 23:11:14 -0300

    re-estilizando a page projetos

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -4,17 +4,18 @@ export default function About() {
     return (
         <div className="flex justify-center">
             <div className="flex justify-center">
-        <div className="flex flex-wrap justify-center md:justify-start md:space-x-11 py-16 text-gray-200 px-8 w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 text-center bg-purple-700 rounded-2xl m-10">
-          <p className="mt-10 inline-block font-bold bg-black hover:bg-gray-700 text-white py-5 px-12 rounded-lg text-2xl">
-            <Link href="/projetos/apirest">API</Link>
-          </p>
-          <p className="mt-10 inline-block bg-black hover:bg-gray-700 text-white font-bold py-5 px-12 rounded-lg text-2xl">
-            <Link href="/projetos/xesqflix">XesqFlix</Link>
-          </p>
-          <p className="mt-10 inline-block bg-black hover:bg-gray-700 text-white font-bold py-5 px-12 rounded-lg text-2xl">
-            <Link href="https://github.com/jhowk14/Meu_portifolio_em_nextjs">GitHub desse site</Link>
-          </p>
-        </div>
+            <div className="grid grid-cols-1">
+              <p className="mt-10 justify-center flex font-bold bg-black hover:bg-gray-700 text-white py-5 px-12 rounded-lg text-2xl">
+                <Link href="/projetos/apirest">API</Link>
+              </p>
+              <p className="mt-10 justify-center flex bg-black hover:bg-gray-700 text-white font-bold py-5 px-12 rounded-lg text-2xl">
+                <Link href="/projetos/xesqflix">XesqFlix</Link>
+              </p>
+              <p className="mt-10 justify-center flex bg-black hover:bg-gray-700 text-white font-bold py-5 px-12 rounded-lg text-2xl">
+                <Link href="https://github.com/jhowk14/Meu_portifolio_em_nextjs">GitHub desse site</Link>
+              </p>
+            </div>
+
       </div>
         </div>
       
