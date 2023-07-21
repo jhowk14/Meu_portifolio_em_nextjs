8d4324954c61f2ccd711b2316ba8cd71843e9c9a
jhowk14 <jonathanperozi16@gmail.com>
2023-07-21 16:36:49 -0300

    head update

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+ 
+function IndexPage() {
+  return (
+    <>
+      <title>Portifolio Jonathan Lourenço</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Portifolio criado em Nextjs por Jonathan Henrique Perozi Lourenço" />
+    </>
+    
+  )
+}
+ 
+export default IndexPage
